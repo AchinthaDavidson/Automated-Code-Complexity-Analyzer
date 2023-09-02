e682f42ef5faeef982ab095e341c14b6f69dae60
IT21209352 <it21209352@my.sliit.lk>
2023-09-02 08:46:15 +0530

    expermient

--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -42,112 +42,142 @@ alert(code)
       reader.readAsText(file);
     });
   };
+  function calculateTokenCount(codeLine) {
+    
+    //var oprandCount = countOperands(codeLine);
+    //var methodCount = countMethods(codeLine);
+
+    var opratorCount =  countOperators(codeLine);
+    var stringCount =  countStringLiterals(codeLine)
+    var variableCounts = countVariables(code);
+    var extractedMethods = extractMethods(code);
+
+
+    console.log("------------------------------------------------ " )
+
+    for (const methodName in extractedMethods) {
+      const lineNumbers = extractedMethods[methodName].join(', ');
+      console.log(`Method: ${methodName}, Line Numbers: ${lineNumbers}`);
+    }
+
+
+    // for (const variableName in variableCounts) {
+    //   const count = variableCounts[variableName].count;
+    //   const uniqueLines = [...new Set(variableCounts[variableName].lines)];
+  
+    //   console.log(`${variableName}: Count ${count}, Lines: ${uniqueLines.join(', ')}`);
+    // }
+
+    //console.log("Line is ---------- " + codeLine)
+    //console.log("Operator Count ----- " + opratorCount)
  
-  function handleSearch(cppCode) {
-    // Regular expressions to match various tokens
-    const tokenPatterns = [
-      /(\w+)\s*\(/g,                // Keywords and functions
-      /::/g,                        // :: operator
-      /(\w+)\s+/g,                  // Keywords and identifiers
-      /'.*?'/g,                     // Characters inside single quotes
-      /".*?"/g,                     // Strings inside double quotes
-      /[\[\]]/g,                    // Square brackets
-      /,/g,                         // Comma operator
-      /\bcase\b|\bdefault\b/g,      // case and default in switch
-      /\./g,                        // Dot operator
-      /\n|;|{|}/g,                  // Newlines, semicolons, curly braces
-      /\bendl\b|\\n/g              // Manipulators like endl and "\n"
-  ];
+    //console.log("Variable Count ---------------------" )
+    //console.log("String Count ----- " + stringCount)
+    //console.log("Total Count "+tokenCount+"------------------------------------------------------------")
 
+    //console.log("Operand Count ----- " + oprandCount)
+    //console.log("Method Count ----- " + methodCount)
+    //console.log("Token Count ----- " + tokenCount)
 
-    // Calculate the token count
-    let tokenCount = 0;
-
-     tokenPatterns.forEach(pattern => {
-      const matche = cppCode.match("//");
-        const matches = cppCode.match(pattern);
-        if (matche!='//'){
-          if (matches) {
-            tokenCount += matches.length;
-        }
-        }
-         
-    });
-
-    return tokenCount;
+    return stringCount;
 }
-  // function handleSearch(cppCode) {
-  //   // Regular expressions to match various tokens
-  //   const tokenPatterns = [
-  //     /(\w+)\s*\(/g,                // Keywords and functions
-  //     /::/g,                        // :: operator
-  //     /(\w+)\s+/g,                  // Keywords and identifiers
-  //     /'.*?'/g,                     // Characters inside single quotes
-  //     /".*?"/g,                     // Strings inside double quotes
-  //     /[\[\]]/g,                    // Square brackets
-  //     /,/g,                         // Comma operator
-  //     /\bcase\b|\bdefault\b/g,      // case and default in switch
-  //     /\./g,                        // Dot operator
-  //     /\n|;|{|}|<<|>>|/g,                  // Newlines, semicolons, curly braces
-  //     /\bendl\b|\\n/g              // Manipulators like endl and "\n"
-  // ];
+
+function countVariables(javaCode) {
+  const variableDeclarationRegex = /\b(?:int|double|float|char|String)\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*(?:=|\b)(?!args\b)/g;
+
+  // Create a map to store the number of times each variable appears and its line numbers.
+  let variableCounts = {};
+
+  // Match variable declarations in the Java code using regex.
+  const variableDeclarations = javaCode.match(variableDeclarationRegex) || [];
+
+  // Match variable usages in the Java code using regex.
+  const variableUsageRegex = /\b[a-zA-Z_][a-zA-Z0-9_]*\b/g;
+  const variableUsages = javaCode.match(variableUsageRegex) || [];
+
+  // Iterate over variable declarations and extract variable names.
+  for (let declaration of variableDeclarations) {
+    const variableName = declaration.split(/\s+/)[1];
+    variableCounts[variableName] = { count: 0, lines: [] };
+  }
+
+  // Iterate over variable usages and update counts and line numbers.
+  const lines = javaCode.split('\n');
+  for (let lineNumber = 0; lineNumber < lines.length; lineNumber++) {
+    const line = lines[lineNumber];
+    const variableUsageMatches = line.match(variableUsageRegex) || [];
+
+    for (let word of variableUsageMatches) {
+      if (variableCounts.hasOwnProperty(word)) {
+        variableCounts[word].count++;
+        variableCounts[word].lines.push(lineNumber + 1);
+      }
+    }
+  }
+
+  // Remove "args" from the variableCounts map
+  delete variableCounts["args"];
+
+  // Return the map of variable counts and line numbers.
+  return variableCounts;
+}
+
+function countOperators(codeLine) {
+
+  const regexPattern = /[!=<>+\-*/%&|^~]+/g;
+  var operators = codeLine.match(regexPattern) || [];
+  var numberOfOperators = operators.length;
+  var hasNumericalValue = /[0-9]/g.test(codeLine);
+  if (hasNumericalValue) {
+    numberOfOperators -= operators.filter(operator => operator === '-').length;
+  }
+  return numberOfOperators;
+}
+
+function countStringLiterals(codeLine) {
+  const regexPattern = /("[^"]*")|('[^']*')/g;
+  const matches = codeLine.match(regexPattern) || [];
+  return matches.length;
+}
 
 
+function extractMethods(javaCode) {
+  // Define a regular expression pattern to match method declarations
+  const methodDeclarationRegex = /(?:public|private|protected|static|final)?\s+(?:\w+\s+)?\w+\s+(\w+)\s*\([^)]*\)\s*(?:throws\s+\w+(?:,\s*\w+)*)?\s*{/g;
 
-  //   // Calculate the token count
-  //   let tokenCount = 0;
+  // Match method declarations in the Java code using regex
+  const methodDeclarations = javaCode.match(methodDeclarationRegex) || [];
 
-  //   tokenPatterns.forEach(pattern => {
-  //     const matche = cppCode.match("//");
-  //       const matches = cppCode.match(pattern);
-  //       if (matche!='//'){
-  //         if (matches) {
-  //           tokenCount += matches.length;
-  //       }
-  //       }
-         
-  //   });
+  // Create an object to store method information
+  const methodInfo = {};
 
-  //   return tokenCount;
+  // Split Java code into lines
+  const lines = javaCode.split('\n');
+
+  // Iterate over method declarations and extract method names and line numbers
+  methodDeclarations.forEach((declaration) => {
+    const methodName = declaration.match(/(\w+)\s*\(/)[1];
+    const lineNumber = lines.findIndex((line) => line.includes(declaration)) + 1;
+
+    if (!methodInfo[methodName]) {
+      methodInfo[methodName] = [];
+    }
+    methodInfo[methodName].push(lineNumber);
+  });
+
+  return methodInfo;
+}
+
+// function countMethods(javaCode) {
+//   // Define a regular expression pattern to match method declarations
+//   const methodDeclarationRegex = /(?:public|private|protected|static|final)?\s+(?:\w+\s+)?\w+\s+(\w+)\s*\([^)]*\)\s*(?:throws\s+\w+(?:,\s*\w+)*)?\s*{/g;
+
+//   // Match method declarations in the Java code using regex
+//   const methodDeclarations = javaCode.match(methodDeclarationRegex) || [];
+
+//   // Return the count of methods
+//   return methodDeclarations.length;
 // }
-
-// function calculateWCC(codeSnippet) {
-//   // Tokenization based on guidelines
-//   const tokens = codeSnippet
-//     .replace(/(["'])(?:\\\1|.)*?\1/g, '') // Remove content inside quotes
-//     .split(/[,\s]/)
-//     .filter(token => token.length > 0);
-
-//   // Token weight mapping
-//   const tokenWeights = {
-//     operators: ['+', '-', '*', '/', '=', '==', '===', '!=', '!==', '<', '>', '<=', '>='],
-//     keywords: ['if', 'else', 'for', 'while', 'do', 'switch', 'case', 'default', 'catch', 'return','void'],
-//     manipulators: ['endl', '\\n'],
-//     brackets: ['(', ')', '{', '}', '[', ']','.'],
-//   };
-
-//   // Calculate token size
-//   let wcc = 0;
-//   for (const token of tokens) {
-//     let tokenSize = 0; // Default token size is 1
-
-//     // Check for operators, keywords, manipulators, brackets
-//     if (tokenWeights.operators.includes(token)) {
-//       tokenSize = 2;
-//     } else if (tokenWeights.keywords.includes(token)) {
-//       tokenSize = 1;
-//     } else if (tokenWeights.manipulators.includes(token)) {
-//       tokenSize = 2;
-//     } else if (tokenWeights.brackets.includes(token)) {
-//       tokenSize = 1;
-//     }
-
-//     wcc += tokenSize;
-//   }
-
-//   return wcc;
-// }
-
 
 
   return (
@@ -184,7 +214,7 @@ alert(code)
         <div style={{background:"white"}}>
         <code >
               {codeLines.map((line, index) => (
-                <div key={index}>{line} ........... {calculateWCC(line)}</div>
+                <div key={index}>{line} ........... {calculateTokenCount(line)}</div>
               ))}
           </code>
 
