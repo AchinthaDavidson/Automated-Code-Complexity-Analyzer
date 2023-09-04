fd223255a13dab57d30b60fca388ed248fd605b1
XYWINGS <it21209352@my.sliit.lk>
2023-09-04 00:07:10 -0700

    wn and wc done

--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -9,6 +9,7 @@ function Home() {
   const [codeLines, setCodeLines] = useState([]);
   const [detectedLanguage, setDetectedLanguage] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [cw,setcw] = useState();
   // get cord into line
   const handleCodeChange = (newCode) => {
     setCode(newCode);
@@ -53,9 +54,12 @@ alert(code)
     var stringnNumCount = countStringLiteralsAndNumerals(code);
     var controlWordCount = countControlWords(code);
     var mergedCounts = {}; // Create an object to store counts by line number
-    var wc = findControlStatements(code)
-    wc.sort ( (a,b) => a.start - b.start);
-    var assingLevel = assignLevelsToCode(wc, lines.length,code)
+   
+    var controlStatments  = findControlStatements(code)
+    controlStatments.sort ( (a,b) => a.start - b.start);
+ 
+    var assingLevel = assignLevelsToCode(controlStatments,code)
+
 
     // Merge counts for String Literals and Numerals
     stringnNumCount.forEach(item => {
@@ -254,64 +258,62 @@ alert(code)
               
           };
       }
-      mergedCounts[lineNumber].W_nesting+= item.level;
-      mergedCounts[lineNumber].W_total += item.level;
+      mergedCounts[lineNumber].W_nesting+= item.wn;
+      mergedCounts[lineNumber].W_control+= item.wc;
+      mergedCounts[lineNumber].W_total += item.wn + item.wc;
   });
 
     
     console.log("Merged Counts by Line Number:");
-    for (const lineNumber in mergedCounts) {
-      const line = lines[lineNumber];
-      const counts = mergedCounts[lineNumber];
-      console.log(`Line Number: ${counts.lineNumber}, Total Count: ${counts.total}`);
-      console.log(line)
-      console.log(`  Literal Count: ${counts.literalCount}`);
-      console.log(`  Method Call Count: ${counts.methodCallCount}`);
-      console.log(`  Class Call Count: ${counts.classCallCount}`);
-      console.log(`  Operator Count: ${counts.operatorCount}`);
-      console.log(`  Variable Count: ${counts.variableCount}`);
-      console.log(`  Method Count: ${counts.methodCount}`);
-      console.log(`  Control Word Count: ${counts.controlWordCount}`);
-      console.log(`  W Control Count: ${counts.W_control}`);
-      console.log(`  W Inheritance Count: ${counts.W_inheritance}`);
-      console.log(`  W Nesting Count: ${counts.W_nesting}`);
-      console.log(`  W Total Count: ${counts.W_total}`);
-      console.log(`  WC Count: ${counts.WC_Value}`);
-      
-    }
+    
+const tableData = [];
 
+for (const lineNumber in mergedCounts) {
+  const counts = mergedCounts[lineNumber];
+  var codeString = lines[counts.lineNumber].replace(/\t/g, '    ').replace(/\r/g, '');
+  const lineData = {
+    'Line': codeString,
+    'S Count': counts.total,
+    'W Control ': counts.W_control,
+    'W Inheritance': counts.W_inheritance,
+    'W Nesting ': counts.W_nesting,
+    'W Total Count': counts.W_total,
+    'WC Count': counts.W_total * counts.total
+  };
+  tableData.push(lineData);
+}
 
-
-
+console.table(tableData);
 
 }
 
-function assignLevelsToCode(controlStructures, codeLength,javaJode) {
+
+function assignLevelsToCode(controlStructuresArray, javaCode) {
   const lines = javaCode.split("\n");
-  
-  const codeLevels = Array(codeLength).fill(0);
+  const codeLevels = [];
 
-  for (const structure of controlStructures) {
-    for (let i = structure.start; i <= structure.end; i++) {
-      codeLevels[i] = structure.level;
+  lines.forEach((line, lineNumber) => {
+    let wn = 0;
+    let wc = 0;
+
+    controlStructuresArray.forEach((controlStructure) => {
+      if (lineNumber >= controlStructure.start  && lineNumber <= controlStructure.end ) {
+        wn += controlStructure.wn || 0;
+        wc += lineNumber === controlStructure.start  ? (controlStructure.wc || 0) : 0;
+      }
+    });
+
+    // Check if the line contains only brackets, comments, or else statements with brackets
+    if (/^(\s*[\{\}\[\]();]|\/\/|\/\*|\*\/|else\s*{)/.test(line)) {
+      wn = 0;
+      wc = 0;
     }
-  }
 
-  const result = codeLevels.map((level, lineNumber) => ({ lineNumber, level }));
-  return result;
+    codeLevels.push({ lineNumber, wn, wc });
+  });
+
+  return codeLevels;
 }
-
-// function assignLevelsToCode(controlStructures, codeLength) {
-//   const codeLevels = Array(codeLength).fill(0);
-
-//   for (const structure of controlStructures) {
-//     for (let i = structure.start; i <= structure.end; i++) {
-//       codeLevels[i] = structure.level;
-//     }
-//   }
-
-//   return codeLevels;
-// }
 
 
 function findControlStatements(javaCode) {
@@ -336,10 +338,21 @@ function findControlStatements(javaCode) {
     let match = line.match(controlStatementRegex);
 
     if (match) {
-      // Push the type and the start line to the stack
+      // Initialize the wc attribute
+      let wc = 0;
+
+      // Check if it's an 'if' condition
+      if (match[1] === "if") {
+        wc = 1;
+      } else if (match[1] === "for" || match[1] === "while") {
+        wc = 2;
+      }
+
+      // Push the type, start line, and wc to the stack
       stack.push({
         type: match[1],
-        start: i ,
+        start: i,
+        wc: wc, // Add the 'wc' attribute
       });
     }
 
@@ -363,8 +376,9 @@ function findControlStatements(javaCode) {
           result.push({
             type: top.type,
             start: top.start,
-            end: i ,
-            level: stack.length // The level is equal to the length of the stack
+            end: i,
+            wn: stack.length, // The level is equal to the length of the stack
+            wc: top.wc, // Add the 'wc' attribute to the result
           });
           stack.pop();
         }
@@ -372,381 +386,8 @@ function findControlStatements(javaCode) {
     }
   }
 
-  // Return the result array
   return result;
 }
-
-
-// function findControlStatementskkkkkkkkkk(javaCode) {
-//   // Initialize the result array
-//   let result = [];
-
-//   // Split the code by line breaks
-//   let lines = javaCode.split(/\r?\n/);
-
-//   // Use regular expressions to match control statements
-//   let controlStatementRegex = /^\s*(if|else if|else|for|while|do|switch|case|default)\s*\(.+\)\s*\{/;
-
-//   // Use a stack to keep track of the current control statements and their lines
-//   let stack = [];
-
-//   // Loop through the lines
-//   for (let i = 0; i < lines.length; i++) {
-//     // Get the current line
-//     let line = lines[i];
-
-//     // Check if the line matches a control statement
-//     let match = line.match(controlStatementRegex);
-
-//     if (match) {
-//       // Push the type and the start line to the stack
-//       stack.push({
-//         type: match[1],
-//         start: i + 1,
-//       });
-//     }
-
-//     // Check if the line contains an opening brace
-//     if (line.includes("{")) {
-//       // Increment the brace count for the top statement on the stack
-//       let top = stack[stack.length - 1];
-//       if (top) {
-//         top.braces = (top.braces || 0) + 1;
-//       }
-//     }
-
-//     // Check if the line contains a closing brace
-//     if (line.includes("}")) {
-//       // Decrement the brace count for the top statement on the stack
-//       let top = stack[stack.length - 1];
-//       if (top) {
-//         top.braces--;
-//         // If the brace count reaches zero, pop the statement from the stack and add it to the result array
-//         if (top.braces === 0) {
-//           result.push({
-//             type: top.type,
-//             start: top.start,
-//             end: i + 1,
-//           });
-//           stack.pop();
-//         }
-//       }
-//     }
-//   }
-
-//   // Return the result array
-//   return result;
-// }
-
-
-
-
-
-
-// function findControlStatements(javaCode) {
-//   // Initialize the result array
-//   let result = [];
-
-//   // Split the code by line breaks
-//   let lines = javaCode.split(/\r?\n/);
-
-//   // Use regular expressions to match control statements
-//   let controlStatementRegex = /^\s*(if|else if|else|for|while|do|switch|case|default)\s*\(.+\)\s*\{/;
-
-//   // Use a stack to keep track of the current control statements and their lines
-//   let stack = [];
-
-//   // Loop through the lines
-//   for (let i = 0; i < lines.length; i++) {
-//     // Get the current line
-//     let line = lines[i];
-
-//     // Check if the line matches a control statement
-//     let match = line.match(controlStatementRegex);
-
-//     if (match) {
-//       // Push the type and the start line to the stack
-//       stack.push({
-//         type: match[1],
-//         start: i + 1,
-//       });
-//     }
-
-//     // Check if the line contains an opening brace
-//     if (line.includes("{")) {
-//       // Increment the brace count for the top statement on the stack
-//       let top = stack[stack.length - 1];
-//       if (top) {
-//         top.braces = (top.braces || 0) + 1;
-//       }
-//     }
-
-//     // Check if the line contains a closing brace
-//     if (line.includes("}")) {
-//       // Decrement the brace count for the top statement on the stack
-//       let top = stack[stack.length - 1];
-//       if (top) {
-//         top.braces--;
-//         // If the brace count reaches zero, pop the statement from the stack and add it to the result array
-//         if (top.braces === 0) {
-//           result.push({
-//             type: top.type,
-//             start: top.start,
-//             end: i + 1,
-//           });
-//           stack.pop();
-//         }
-//       }
-//     }
-//   }
-
-//   // Return the result array
-//   return result;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function identifyControlStructures(code) {
-//   const lines = code.split('\n');
-//   const result = [];
-
-//   let sequentialCount = 0;
-//   let branchCount = 0;
-//   let iterativeCount = 0;
-//   let switchCount = 0;
-//   let insideSwitch = false;
-
-//   for (let i = 0; i < lines.length; i++) {
-//       const line = lines[i].trim();
-//       let lineResult = {
-//           LineNumber: i ,
-//           Sequential: 0,
-//           Branch: 0,
-//           Iterative: 0,
-//           Switch: 0,
-//       };
-
-//       if (line.startsWith('if') || line.startsWith('else if') || line.startsWith('else')) {
-//           lineResult.Branch++;
-//           branchCount++;
-//       } else if (line.startsWith('while') || line.startsWith('for')) {
-//           lineResult.Iterative++;
-//           iterativeCount++;
-//       } else if (line.startsWith('switch')) {
-//           lineResult.Switch++;
-//           insideSwitch = true;
-//           switchCount++;
-//       } else if (insideSwitch && line.startsWith('case')) {
-//           lineResult.Switch++;
-//           switchCount++;
-//       } else if (insideSwitch && line.startsWith('default:')) {
-//           lineResult.Switch++;
-//           switchCount++;
-//       } else if (insideSwitch && line.startsWith('}')) {
-//           insideSwitch = false;
-//       } else {
-//           lineResult.Sequential++;
-//           sequentialCount++;
-//       }
-
-//       result.push(lineResult);
-//   }
-
-//   return result
-// }
-
-
-// async function classParser(javaCode){
-//   let lines = javaCode.split(/\r?\n/);
-//   var classData = [];
-//   var lineLength = lines.length 
-
-//   let selectedLines = lines.slice(0, lineLength); 
-
-//   while(lineLength > 0){
-
-//     var classDataResult =  findJavaClasses(selectedLines);
-//     console.log(classDataResult)
-
-//     if(classDataResult[0]==null){
-//       break
-//     }else{
-//       var newStart = classDataResult[0].start + 1
-//       var newEnd = classDataResult[0].end - 1
-//       lineLength = newEnd - newStart
-//       if(lineLength <= 0){
-//         break
-//       }else{
-//         selectedLines = lines.slice(newStart, newEnd); 
-//       }
-//     }
-//   }
-// }
-
-// function findJavaClasses(javaCode) {
-//   // Initialize the result object
-//   let result = []
-
-//   // Split the code by line breaks
-//   let lines = javaCode.split(/\r?\n/);
-
-//   // Use a regular expression to match a class declaration
-//   let classRegex = /^\s*(?:public|protected|private|abstract|static|final|strictfp)?\s*class\s+(\w+)\s*(?:extends\s+\w+\s*)?(?:implements\s+[\w\s,]+)?\s*\{/;
-
-//   // Use a stack to keep track of the nested classes
-//   let stack = [];
-
-//   // Loop through the lines
-//   for (let i = 0; i < lines.length; i++) {
-//     // Get the current line
-//     let line = lines[i];
-
-//     // Check if the line matches a class declaration
-//     let match = line.match(classRegex);
-
-//     // If there is a match, push the class name and the declaration line to the stack
-//     if (match) {
-//       stack.push({
-//         name: match[1],
-//         start: i 
-//       });
-//     }
-
-//     // Check if the line contains an opening brace
-//     if (line.includes("{")) {
-//       // Increment the brace count for the top class on the stack
-//       let top = stack[stack.length - 1];
-//       if (top) {
-//         top.braces = (top.braces || 0) + 1;
-//       }
-//     }
-
-//     // Check if the line contains a closing brace
-//     if (line.includes("}")) {
-//       // Decrement the brace count for the top class on the stack
-//       let top = stack[stack.length - 1];
-//       if (top) {
-//         top.braces--;
-//         // If the brace count reaches zero, pop the class from the stack and add it to the result object
-//         if (top.braces === 0) {
-//           result.push({
-//             name: top.name,
-//             start: top.start,
-//             end: i 
-//           });
-//           stack.pop();
-//         }
-//       }
-//     }
-//   }
-
-//   // Return the result object
-//   return result;
-// }
-
-
-
-// function assignWeights(javaCode) {
-//   // Initialize the result array
-//   let result = [];
-
-//   // Split the code by line breaks
-//   let lines = javaCode.split(/\r?\n/);
-
-//   // Use regular expressions to match different patterns of Java syntax
-//   let classRegex = /^\s*(?:public|protected|private|abstract|static|final|strictfp)?\s*class\s+(\w+)\s*(?:extends\s+\w+\s*)?(?:implements\s+[\w\s,]+)?\s*\{/;
-//   let methodRegex = /^\s*(?:public|protected|private|abstract|static|final|strictfp|synchronized)?\s*(?:\w+\s+)?(\w+)\s*\((?:[\w\s,]+)?\)\s*(?:throws\s+[\w\s,]+)?\s*\{/;
-//   let blockRegex = /^\s*(?:if|else if|else|for|while|do while|switch|case|default)\s*(?:\([\w\s,]+\))?\s*\{/;
-//   let commentRegex = /^\s*\/\//;
-
-//   // Use a stack to keep track of the current nested classes and their weights
-//   let stack = [];
-
-//   // Loop through the lines
-//   for (let i = 0; i < lines.length; i++) {
-//     // Get the current line
-//     let line = lines[i];
-
-//     // Check if the line matches a class declaration
-//     let classMatch = line.match(classRegex);
-
-//     // If there is a match, push the class name and its weight to the stack
-//     if (classMatch) {
-//       stack.push({
-//         name: classMatch[1],
-//         weight: stack.length // The weight is equal to the length of the stack
-//       });
-//     }
-
-//     // Check if the line matches a method or a block declaration
-//     let methodMatch = line.match(methodRegex);
-//     let blockMatch = line.match(blockRegex);
-
-//     // If there is a match, increment the weight for the top class on the stack
-//     if (methodMatch || blockMatch) {
-//       let top = stack[stack.length - 1];
-//       if (top) {
-//         top.weight++;
-//       }
-//     }
-
-//     // Check if the line matches a comment
-//     let commentMatch = line.match(commentRegex);
-
-//     // If there is a match, do nothing
-//     if (commentMatch) {
-//       // Do nothing
-//     } else {
-//       // Add the current line and its weight to the result array
-//       result.push({
-//         lineNumber: i + 1,
-//         weight: stack.length > 0 ? stack[stack.length - 1].weight : 0
-//       });
-//     }
-
-//     // Check if the line contains an opening brace
-//     if (line.includes("{")) {
-//       // Increment the brace count for the top class on the stack
-//       let top = stack[stack.length - 1];
-//       if (top) {
-//         top.braces = (top.braces || 0) + 1;
-//       }
-//     }
-
-//     // Check if the line contains a closing brace
-//     if (line.includes("}")) {
-//       // Decrement the brace count for the top class on the stack
-//       let top = stack[stack.length - 1];
-//       if (top) {
-//         top.braces--;
-//         // If the brace count reaches zero, pop the class from the stack and decrement its weight
-//         if (top.braces === 0) {
-//           top.weight--;
-//           stack.pop();
-//         }
-//       }
-//     }
-//   }
-
-//   // Return the result array
-//   return result;
-// }
 
 
 function countLines(text) {
